527a3d1d527a093573baba82c97cadfd44d8273a
joweee <joel.aguirre.7e7@itb.cat>
2025-02-03 20:53:37 +0100

    Inician Las Aventuras Fantásticas De Cubi El Cubo (Es daltonico)

---
+++ Assets/Scripts/colores.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "color", menuName = "NuevoColor")]
+public class colores : ScriptableObject
+{
+    public Color ColorBase;
+    public Color Daltonico;
+}
